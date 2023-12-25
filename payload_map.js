48768adbc22b7529cd4597411be45578f7d4d6b3
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2023-12-25 11:13:46 +0100

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -1,9 +1,9 @@
 const payload_map =
     [
         {
-            displayTitle: 'etaHEN 1.1b',
+            displayTitle: 'etaHEN 1.2b',
             description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 4.03 & 4.50 Only.',
-            fileName: 'etaHEN-1.1b.bin',
+            fileName: 'etaHEN-1.2B.bin',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
             version: '1.1 beta'
