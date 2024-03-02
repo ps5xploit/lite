45eb174816e4a55ad459945a651307b423707214
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2024-03-02 19:00:25 +0100

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,12 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN',
+            description: 'FPKG enabler ',
+            fileName: 'etaHEN-1.7B.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.7 beta'
+        },
+
+    ];
